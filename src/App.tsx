65e32530156ec5c381d46d13960feae8318d871c
jiwonmik <jiwonmik@gmail.com>
2023-02-22 22:57:02 +0900

    feat: use nested Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useRoutes } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
@@ -7,30 +7,16 @@ import NotFound from "./pages/NotFound";
 
 
 function App() {
-
-  let routes = useRoutes([
-    {
-      path: "/",
-      element: <Layout />,
-      errorElement: <NotFound />,
-      children: [
-        {
-          path: "todo",
-          element: <Todo />,
-        },
-        {
-          path: "signin",
-          element: <SignIn />,
-        },
-        {
-          path: "signup",
-          element: <SignUp />,
-        },
-      ],
-    },
-  ]);
-
-  return routes;
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/signin" element={<SignIn/>} />
+      <Route path="/todo" element={<Todo/>} />
+      <Route path="/*" element={<NotFound/>} />
+      <Route path="/" element={<Layout/>} />
+    </Routes>
+  )
+  
 };
 
 export default App;
