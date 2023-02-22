1b986a55a1b7cb530ed929bb0976b15a3214ec3e
jiyeon <a1919361@gmail.com>
2023-02-23 08:38:14 +0900

    feat : tailwind 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="container mx-auto flex flex-col items-center">
+      <h1 className="text-2xl">todo app</h1>
+    </div>
+  );
 }
 
 export default App;
