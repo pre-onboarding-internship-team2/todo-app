372b4bf04b441c4d66a808b41f8ffca67a73a820
ggsno <ogs0518@gmail.com>
2023-02-23 00:04:07 +0900

    feat: 로그인 여부에 따른 리다이렉트 처리 - 토큰 있는 상태로 /signin, /signup 접근 시 todo로 리다이렉트 - 토큰 없는 상태로 /todo 접근시 /signin으로 리다이렉트

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,36 @@ import SignInPage from "pages/signin";
 import SignUpPage from "pages/signup";
 import { Navigate } from "react-router-dom";
 import TodoPage from "pages/todo";
+import { useAuth } from "context/authContext";
 
 function App() {
+  const { hasAuth } = useAuth();
+
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Navigate to={"/signin"} />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/todo" element={<TodoPage />} />
+        <Route
+          path="/"
+          element={
+            hasAuth ? (
+              <Navigate to={"/todo"} replace />
+            ) : (
+              <Navigate to={"/signin"} replace />
+            )
+          }
+        />
+        <Route
+          path="/signup"
+          element={hasAuth ? <Navigate to={"/todo"} replace /> : <SignUpPage />}
+        />
+        <Route
+          path="/signin"
+          element={hasAuth ? <Navigate to={"/todo"} replace /> : <SignInPage />}
+        />
+        <Route
+          path="/todo"
+          element={hasAuth ? <TodoPage /> : <Navigate to={"/signin"} replace />}
+        />
       </Route>
     </Routes>
   );
