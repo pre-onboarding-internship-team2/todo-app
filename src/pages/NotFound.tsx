a463332e21340a5998935e1fe9e15b07e5303f0f
foreknowledge <kyj_0129@naver.com>
2023-02-23 19:12:21 +0900

    feat: NotFound 페이지 UI 보강

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,9 @@
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <div className='flex h-screen items-center justify-center '>
+      <h1 className='text-3xl font-bold'>요청하신 페이지를 찾을 수 없습니다 😞</h1>
+    </div>
+  );
 };
 
 export default NotFound;
