2015aa0058fb1b052068691c8a65a813f0bdba48
jiwonmik <jiwonmik@gmail.com>
2023-02-23 13:57:04 +0900

    feat: add Home button in Todo Page

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Todo(){
     return (
-        <h2>Todo Page</h2>
+        <>
+            <h2>Todo Page</h2>
+            <Link to="/" replace>
+                <button>Home</button>
+            </Link>
+        </>
     );
 };
 
