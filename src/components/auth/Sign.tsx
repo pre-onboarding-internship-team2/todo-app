d4bcd2822b21a1a23e492ffc2eaff5addbeff949
jiwonmik <jiwonmik@gmail.com>
2023-02-23 11:26:38 +0900

    feat: checkValidForm function

--- src/components/auth/Sign.tsx
+++ src/components/auth/Sign.tsx
@@ -13,11 +13,21 @@ function Sign({submitAction, submitCallback} : {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
+        console.log("submit");
         submitAction({email, password}, submitCallback);
     };
 
+    useEffect(() => {
+        const checkValidForm = () => {
+            const EMAIL_CHECK = new RegExp("@");
+            const PW_CHECK = new RegExp("(?=.{8,})");
 
+            return (EMAIL_CHECK.test(email) && PW_CHECK.test(password));
+        };
+    
+        setIsValidForm(checkValidForm());
+      }, [email, password]);
+    console.log(isValidForm);
     return (
         <div className="flex justify-center items-center">
             <form className="flex flex-col align"
@@ -34,8 +44,15 @@ function Sign({submitAction, submitCallback} : {
                     type="password"
                     onChange={(e) => setPassword(e.target.value)} 
                     placeholder="Password"/>
-                <button className="my-0.5 w-60 text-lg py-2 bg-green-300 text-white rounded-2xl"
+                { !isValidForm ? 
+                    <div className="flex flex-col justify-center items-center">
+                        <h2 className="text-red-500	text-sm items-center justify-center">@를 포함한 이메일을 입력해주세요</h2>
+                        <h2 className="text-red-500	text-sm items-center justify-center">비밀번호는 8자리 이상 입력해주세요</h2>
+                    </div> 
+                        : null}
+                <button className="my-0.5 w-60 text-lg py-2 bg-green-300 text-white rounded-2xl disabled:bg-gray-300 disabled:cursor-not-allowed"
                     data-testid="signin-button"
+                    disabled={!isValidForm}
                 >Submit</button>   
             </form>
         </div>
