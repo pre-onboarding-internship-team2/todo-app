c88994d93bec048d04c7526d7ab1b2fc6934222d
jiwonmik <jiwonmik@gmail.com>
2023-02-23 11:01:30 +0900

    refactor: Layout file

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -17,28 +17,27 @@ const Home = () => {
     console.log(auth);
     return (
         <>
-    <div className="flex flex-col justify-center items-center place-items-center h-screen">
-        <h1 className="mb-10 text-2xl">My To Do</h1>
-        {!auth.authInfo ? (
-          <>
-          <button className="my-0.5 w-60 text-lg py-2 bg-green-300 text-white rounded-2xl">
-            <Link to="/signin">로그인</Link>
-          </button>
-          <button className="my-0.5 w-60 text-lg py-2	bg-green-300 text-white rounded-2xl">
-            <Link to="/signup">회원가입</Link>
-        </button>
-          </>
-        ) : (
-          <button 
-            onClick={() => {
-              auth.signOut(() => navigate("/signin"));
-            }}
-            className={"w-20"}
-          >
-            로그아웃
-          </button>
-        )}
-      </div>
+            <div className="flex flex-col justify-center items-center place-items-center h-screen">
+                <h1 className="mb-10 text-2xl">My To Do</h1>
+                    {!auth.authInfo ? (
+                        <>
+                            <button className="my-0.5 w-60 text-lg py-2 bg-green-300 text-white rounded-2xl">
+                                <Link to="/signin">로그인</Link>
+                            </button>
+                            <button className="my-0.5 w-60 text-lg py-2	bg-green-300 text-white rounded-2xl">
+                                <Link to="/signup">회원가입</Link>
+                            </button>
+                        </>
+                    ) : (
+                        <button 
+                            onClick={() => {
+                            auth.signOut(() => navigate("/signin"));
+                            }}
+                            className={"w-20"}>
+                            로그아웃
+                        </button>
+                    )}
+            </div>
         </>
     );
 };
