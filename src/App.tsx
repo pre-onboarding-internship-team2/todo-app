829df2c5b1235f91ae4020e7ba43374149b95128
Hyuksang-Oh <iuesver@gmail.com>
2023-02-23 21:01:45 +0900

    refactor : createBrowserRouter App.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { SignInPage } from "./pages/SignInPage";
+import { SignUpPage } from "./pages/SignUpPage";
+import { TodoPage } from "./pages/TodoPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { path: "", element: <Navigate to="signin" /> },
+      {
+        path: "signin",
+        index: true,
+        element: <SignInPage />,
+      },
+      {
+        path: "signup",
+        element: <SignUpPage />,
+      },
+      {
+        path: "todo",
+        element: <TodoPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return <Outlet />;
--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-import App from "./App";
-import { NotFoundPage } from "./pages/NotFoundPage";
-import { SignInPage } from "./pages/SignInPage";
-import { SignUpPage } from "./pages/SignUpPage";
-import { TodoPage } from "./pages/TodoPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFoundPage />,
-    children: [
-      { path: "", element: <Navigate to="signin" /> },
-      {
-        path: "signin",
-        index: true,
-        element: <SignInPage />,
-      },
-      {
-        path: "signup",
-        element: <SignUpPage />,
-      },
-      {
-        path: "todo",
-        element: <TodoPage />,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
