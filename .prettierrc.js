9b88b866998e45ce3b38e315e606c6c1e8747935
Eunseo Choi <yses9296@gmail.com>
2023-02-23 19:56:58 +0900

    feat prettier 설치 및 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 100, // 기본값 80
+    tabWidth: 2,
+    singleQuote: true, // 기본값 false
+    trailingComma: "es5",
+    semi: true,
+    useTabs: false,
+    arrowParens: "always",
+    bracketSpacing: "true",
+    bracketSameLine: "false",
+};
