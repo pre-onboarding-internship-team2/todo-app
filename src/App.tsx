a55ae5f8c984a09475903d594fad6d3fb0ce5fac
onezeun <onezeun@gmail.com>
2023-02-23 15:26:25 +0900

    feat: eslint, prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { Navigate, useRoutes } from "react-router-dom";
+import { Navigate, useRoutes } from 'react-router-dom';
 
-import Layout from "components/layout/Layout";
-import SignIn from "components/auth/SignIn";
-import SignUp from "components/auth/SignUp";
-import Todo from "components/todo/Todo";
+import Layout from 'components/layout/Layout';
+import SignIn from 'components/auth/SignIn';
+import SignUp from 'components/auth/SignUp';
+import Todo from 'components/todo/Todo';
 
 function App() {
-  const isAuth = localStorage.getItem("user");
+  const isAuth = localStorage.getItem('user');
 
   let element = useRoutes([
     {
-      path: "/",
+      path: '/',
       element: <Layout />,
       children: [
         {
-          path: "",
+          path: '',
           element: <Navigate to="todo" />,
         },
         {
-          path: "todo",
+          path: 'todo',
           element: isAuth ? <Todo /> : <Navigate to="/signin" />,
         },
         {
-          path: "signin",
+          path: 'signin',
           element: !isAuth ? <SignIn /> : <Navigate to="/todo" />,
         },
         {
-          path: "signup",
+          path: 'signup',
           element: !isAuth ? <SignUp /> : <Navigate to="/todo" />,
         },
       ],
     },
   ]);
-  return element
-};
+  return element;
+}
 export default App;
