316350218ee83b7500ef10a2ad3eb58244d38be0
jiwonmik <jiwonmik@gmail.com>
2023-02-24 09:37:17 +0900

    feat: Todo Page

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
+import TodoCreate from "../components/todo/TodoCreate";
+import TodoHead from "../components/todo/TodoHead";
+import TodoList from "../components/todo/TodoList";
+import { TodoProvider } from "../context/todo";
 
 function Todo(){
     return (
-        <div className="flex flex-col m-5 justify-center items-center">
-            <Link to="/" replace>
-                <button className="w-20 absolute right-6 bg-green-1 text-white my-0.5 w-60 text-base py-2 rounded-2xl hover:bg-green-2">
-                    Home
-                </button>
-            </Link>
-            <h2>Todo Page</h2>
-        </div>
+        <TodoProvider>
+            <div className="flex flex-col m-5 justify-center items-center">
+                <Link to="/" replace>
+                    <button className="w-28 absolute right-6 bg-green-1 text-white my-0.5 w-60 text-base py-2 rounded-2xl hover:bg-green-2">
+                        Home
+                    </button>
+                </Link>
+            </div>
+            <TodoHead/>
+            <TodoList/>
+            <TodoCreate/>
+        </TodoProvider>
     );
 };
 
